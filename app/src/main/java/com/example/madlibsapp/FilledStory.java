e698bda67602a2b0082e08a0b5b3547398b5fa69
cameron_chiu <cameronchiu31@icloud.com>
2019-09-09 11:41:44 -0500

    All working

--- app/src/main/java/com/example/madlibsapp/FilledStory.java
+++ app/src/main/java/com/example/madlibsapp/FilledStory.java
@@ -14,7 +14,7 @@ public class FilledStory extends AppCompatActivity {
         setContentView(R.layout.activity_filled_story);
 
         Intent intent = getIntent();
-        String editText = intent.getStringExtra("box1");
+        String editText = intent.getStringExtra("box");
         String editText2 = intent.getStringExtra("box2");
         String editText3 = intent.getStringExtra("box3");
         String editText4 = intent.getStringExtra("box4");
@@ -29,10 +29,10 @@ public class FilledStory extends AppCompatActivity {
 
         String story = "The Mobile App Development class is a(n) "+ editText + " place" +
                 " to say the least. The classmates in period 5 like "+editText2+" and "+editText3
-                +" are always "+editText4+" around in class instead of working on their code."
+                +" are always "+editText4+" in class instead of working on their code."
                 +" But what makes M448 really "+editText+" is the "+editText5+" teacher, Mrs. Denna."
-                +"I mean one time during a lecture, she "+editText6+" and then proceeded to "+editText7+" for the remainder"
-                + "of the period. Everyone in the room sat there quietly until the bell rang. But the next day," +
+                +" I mean one time during a lecture, she "+editText6+"ed and then proceeded to "+editText7+" for the remainder "
+                + "of the period. Everyone in the room sat there quietly until the bell rang. But the next day, " +
                 "something "+editText8+" happened. Mrs. Denna was just, different! She pulled up the notes for our unit, "+editText9
                 +", and started going over it with a completely "+editText10+" voice. Are you "+editText11+"? "+editText12+" asked." ;
 
